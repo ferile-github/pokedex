0b0f85b27ccb6df793cb2d12e180428b3504ce28
Andy - SearchBAR <andy@searchbar.co.za>
2023-07-21 16:06:50 +0100

    Adding pagination

---
+++ src/app/pokemon/[paging]/page.tsx
@@ -0,0 +1,48 @@
+import PokemonItem from '@/components/pokemon/PokemonItem';
+import { getAllPokemon } from '../../../lib/fetch-pokemon';
+import { env } from 'process';
+import Link from 'next/link';
+
+export const metadata = {
+  title: 'Pokedex',
+  description: 'The Pokedex',
+};
+
+export default async function Page({ params }) {
+  const page = parseInt(params.paging);
+  const pagination:Number = parseInt(env.NEXT_PUBLIC_PAGINATION);
+  let nextPage = page + 1;
+  let previousPage = page - 1;
+
+  const offset = pagination * page;
+  const allPokemon = await getAllPokemon(`?offset=${offset}&limit=${pagination}`);
+  const limit = 16;
+
+  return (
+    <>
+      <ol className="flex justify-between mb-lg">
+        <li>
+          {page !== 0 && (
+            <Link className="flex p-2 text-white" href={`${previousPage}`}>
+              Previous Page
+            </Link>
+          )}
+        </li>
+
+        <li>
+          {limit !== nextPage && (
+            <Link className="flex p-2 text-white" href={`${nextPage}`}>
+              Next Page
+            </Link>
+          )}
+        </li>
+      </ol>
+
+      <div className={`grid sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-xs mb-[400px]`}>
+        {allPokemon.results.map((pokemon) => (
+          <PokemonItem key={pokemon.name} pokemon={pokemon} />
+        ))}
+      </div>
+    </>
+  );
+}
--- src/components/pokemon/PokemonItem.tsx
+++ src/components/pokemon/PokemonItem.tsx
@@ -3,42 +3,44 @@
 import { Capitalise, getPokemonID } from '@/utils/utils';
 import Image from 'next/image';
 import styles from '@/styles/modules/Pokemon.module.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getPokemon } from '@/lib/fetch-pokemon';
 import { env } from 'process';
 import PokemonData from './PokemonData';
 
 export default function PokemonItem({ pokemon }) {
   const [pokemonData, setData] = useState(null);
-  const imageUrl = env.NEXT_PUBLIC_SPRITE_URL;
 
-  function portraitURL(url) {
-    return `${imageUrl}/other/dream-world/${getPokemonID(url)}.svg`;
-  }
-
-  async function fetchData() {
-    const data = await getPokemon(pokemon.name);
-    setData(data);
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getPokemon(pokemon.name);
+      setData(data);
+    };
+    fetchData();
+  }, [pokemon.name]);
 
   return (
-    <div className={`${styles.pokemon__item} relative`} key={pokemon.name}>
-      <button onMouseOver={fetchData} className="block w-full">
-        <Image
-          className={`mb-sm ${styles.portrait}`}
-          alt={pokemon.name}
-          height={400}
-          width={400}
-          src={portraitURL(pokemon.url)}
-          priority={true}
-        />
-      </button>
+    <>
+      {pokemonData && (
+        <div className={`${styles.pokemon__item} relative`} key={pokemon.name}>
+          <button className="block w-full">
+            <Image
+              className={`mb-sm ${styles.portrait}`}
+              alt={pokemon.name}
+              height={400}
+              width={400}
+              src={pokemonData.sprites.other.dream_world.front_default}
+              priority={true}
+            />
+          </button>
 
-      <h2 className="h5 mb-0 text-center">
-				#0{getPokemonID(pokemon.url)} - {Capitalise(pokemon.name)}
-			</h2>
+          <h2 className="h5 mb-0 text-center">
+            #0{getPokemonID(pokemon.url)} - {Capitalise(pokemon.name)}
+          </h2>
 
-      {pokemonData && <PokemonData pokemonID={`#0${getPokemonID(pokemon.url)}`} pokemonData={pokemonData} />}
-    </div>
+          <PokemonData pokemonID={`#0${getPokemonID(pokemon.url)}`} pokemonData={pokemonData} />
+        </div>
+      )}
+    </>
   );
 }
